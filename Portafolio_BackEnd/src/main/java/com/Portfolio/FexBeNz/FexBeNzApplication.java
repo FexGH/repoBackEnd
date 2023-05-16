dede999df00c75f24418844969cf52b00f6b5cf0
FexGH <fenix_el_mejor8130@hotmail.com>
2023-05-16 19:52:11 -0300

    a ver el nuevo backend

---
+++ Portafolio_BackEnd/src/main/java/com/Portfolio/FexBeNz/FexBeNzApplication.java
@@ -0,0 +1,13 @@
+package com.Portfolio.FexBeNz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FexBeNzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FexBeNzApplication.class, args);
+	}
+
+}
---
+++ Portafolio_BackEnd/src/test/java/com/Portfolio/FexBeNz/FexBeNzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Portfolio.FexBeNz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FexBeNzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
